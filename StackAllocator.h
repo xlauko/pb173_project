91edc137ba6200d6e4d59d04beb9ebbaa779c2b2
Jiří Novotný <novotng@gmail.com>
2016-04-13 14:39:00 +0200

    StackAllocator changes

--- StackAllocator.h
+++ StackAllocator.h
@@ -6,24 +6,13 @@
 #define PB173_PROJECT_STACKALLOCATOR_H
 #pragma once
 
-#include <cstdio>
-#include <queue>
-#include <assert.h>
 #include "base.h"
-
+#include <cassert>
 
 namespace allocators {
-    template<size_t size>
-    struct StackAllocator {
-
-        StackAllocator() { }
-
-        StackAllocator(const StackAllocator &) = delete;
-        StackAllocator(StackAllocator &&) = delete;
-        StackAllocator &operator=(const StackAllocator &) = delete;
-        StackAllocator &operator=(StackAllocator &&) = delete;
-
-        Block allocate(size_t n) noexcept {
+    template <size_t size> struct StackAllocator {
+        Block allocate(size_t n) noexcept
+        {
             Block blk;
 
             if (_top + n > _data + size || n == 0)
@@ -35,32 +24,30 @@ namespace allocators {
             return blk;
         }
 
-        void deallocate(Block &blk) {
+        void deallocate(Block& blk)
+        {
             assert(isLast(blk));
-            _top = static_cast<char *>(blk.ptr);
+            _top = reinterpret_cast<char*>(blk.ptr);
             blk.reset();
         }
 
-        bool owns(const Block &blk) {
+        bool owns(const Block& blk)
+        {
             return blk.ptr >= _data && blk.ptr <= _data + size;
         }
 
-        bool operator==(const StackAllocator &other) {
-            return false;
-        }
-
-        bool operator!=(const StackAllocator &other) {
-            return false;
-        }
+        bool operator==(const StackAllocator&) { return false; }
+        bool operator!=(const StackAllocator&) { return false; }
 
     private:
         char _data[size];
-        char *_top = _data;
+        char* _top = _data;
 
-        bool isLast(Block &blk) {
-            return static_cast<char*>(blk.ptr) + blk.size == static_cast<char *>(_top);
+        bool isLast(Block& blk)
+        {
+            return reinterpret_cast<char*>(blk.ptr) + blk.size == _top;
         }
     };
 }
 
-#endif //PB173_PROJECT_STACKALLOCATOR_H
+#endif // PB173_PROJECT_STACKALLOCATOR_H
