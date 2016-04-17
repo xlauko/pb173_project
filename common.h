151bf66fa81dab84e41a3e8d8d94d7e5101e0617
xlauko <xlauko@mail.muni.cz>
2016-04-17 16:06:47 +0200

    Added freelist

---
+++ common.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "base.h"
+#include "catch.hpp"
+
+inline void test_block(Block& blk, int n) {
+    REQUIRE(blk.ptr != nullptr);
+    REQUIRE(blk.size == n);
+}
+
+inline void test_null(Block& blk) {
+    REQUIRE(blk.ptr == nullptr);
+    REQUIRE(blk.size == 0);
+}
+
+
