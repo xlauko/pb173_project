d8a3004f7a715a0255388dc9a0d379863eb82689
kejsty <k.kejstova@gmail.com>
2016-04-27 15:23:05 +0200

    Allignment in Stackallocator, solved issue #5

--- StackAllocator.h
+++ StackAllocator.h
@@ -3,18 +3,24 @@
 #include "base.h"
 #include "type_classes.h"
 #include <cassert>
+#include <memory>
 
 namespace allocators {
 template <size_t size> struct StackAllocator : Eq {
-    Block allocate(size_t n) noexcept {
+    Block allocate(size_t n, size_t alignment = 8) noexcept {
         Block blk;
-
         if (_top + n > _data + size || n == 0)
             return blk;
 
-        blk.ptr = _top;
-        blk.size = n;
-        _top = _top + n;
+        size_t space = size - (_top - _data);
+        void* data = static_cast<void*>(_top);
+        if (std::align(alignment, n, data, space)) {
+            blk.ptr = data;
+            _top = static_cast<char*>(data);
+            _top = _top + n;
+            blk.size = n;
+            return blk;
+        }
         return blk;
     }
 
@@ -24,7 +30,9 @@ template <size_t size> struct StackAllocator : Eq {
         blk.reset();
     }
 
-    bool owns(const Block& blk) const { return blk.ptr >= _data && blk.ptr < _top; }
+    bool owns(const Block& blk) const {
+        return blk.ptr >= _data && blk.ptr < _top;
+    }
 
     bool operator==(const StackAllocator& b) const { return _top == b._top; }
 
@@ -32,6 +40,8 @@ private:
     char _data[size];
     char* _top = _data;
 
-    bool isLast(Block& blk) { return reinterpret_cast<char*>(blk.ptr) + blk.size == _top; }
+    bool isLast(Block& blk) {
+        return reinterpret_cast<char*>(blk.ptr) + blk.size == _top;
+    }
 };
 }
