800de62c5b66c78b10eba576cf16576a8781df17
xlauko <xlauko@mail.muni.cz>
2016-05-04 23:47:39 +0200

    Bucket allocator implementation

--- Bucketizer.h
+++ Bucketizer.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cassert>
 #include "base.h"
 #include "type_classes.h"
 
@@ -11,14 +12,41 @@ struct Bucketizer : Eq {
 
     static constexpr size_t number_of_buckets = ((max - min + 1) / step);
 
-    Block allocate(size_t n) {}
+    Bucketizer() = default;
+    Bucketizer(const Bucketizer&) = delete;
+    Bucketizer(Bucketizer&&) = default;
+    Bucketizer& operator=(const Bucketizer&) = delete;
+    Bucketizer& operator=(Bucketizer&&) = default;
 
-    void deallocate(Block& blk) noexcept {}
+    Block allocate(size_t n) {
+        if (bucket_index(n) < number_of_buckets)
+            return get_bucket_allocator(n).allocate(n);
+        return {};
+    }
 
-    bool owns(const Block& blk) const noexcept {}
-    bool operator==(const Bucketizer& b) const {}
+    void deallocate(Block& blk) noexcept {
+        if (!blk)
+            return;
+        static_assert(owns(blk), "Deallocating foreing block");
+        get_bucket_allocator(blk.size).deallocate(blk);
+    }
+
+    bool owns(const Block& blk) const noexcept {
+        return blk && bucket_index(blk.size) < number_of_buckets &&
+               get_bucket_allocator(blk.size).owns(blk);
+    }
+
+    bool operator==(const Bucketizer& b) const {
+        return _buckets == b._buckets;
+    }
 
 private:
+    size_t bucket_index(size_t n) const noexcept { return n / step; }
+    Allocator* get_bucket_allocator(size_t n) const noexcept {
+        assert(min < n && n < max);
+        return &_buckets[bucket_index(n)];
+    }
+
     Allocator _buckets[number_of_buckets];
 };
 }
