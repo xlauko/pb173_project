dc2c846bcff0c26b9440b486df9be76ad5e15030
xlauko <xlauko@mail.muni.cz>
2016-05-04 10:31:02 +0200

    Block extended

--- base.h
+++ base.h
@@ -2,13 +2,26 @@
 
 #include "type_classes.h"
 #include <cstddef>
+#include <memory>
 
 struct Block : Eq {
     void* ptr;
     size_t size;
 
-    Block() : ptr(nullptr), size(0) {}
+    Block() noexcept : ptr(nullptr), size(0) {}
     Block(void* ptr, size_t size) : ptr(ptr), size(size) {}
+    Block(Block&& blk) noexcept { *this = std::move(blk); }
+    Block& operator=(Block&& blk) noexcept {
+        ptr = blk.ptr;
+        size = blk.size;
+        blk.reset();
+        return *this;
+    }
+
+    Block& operator=(const Block& blk) noexcept = default;
+    Block(const Block& blk) noexcept = default;
+
+    ~Block() {}
 
     void reset() {
         ptr = nullptr;
@@ -17,5 +30,7 @@ struct Block : Eq {
 
     operator bool() const { return ptr; }
 
-    bool operator==(const Block& other) const { return other.ptr == ptr; }
+    bool operator==(const Block& other) const {
+        return other.ptr == ptr && size == other.size;
+    }
 };
