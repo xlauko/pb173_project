30e4278a89dbe8cf8603d308b54d275921780685
kejsty <k.kejstova@gmail.com>
2016-05-15 01:22:07 +0200

    added test on move for Mallocator

--- test/test_mallocator.cpp
+++ test/test_mallocator.cpp
@@ -18,3 +18,24 @@ TEST_CASE("Mallocator Test") {
     test_null(block1);
     test_null(block2);
 }
+
+TEST_CASE("Mallocator : move constructor, move operator="){
+    Mallocator  fst;
+    Block block = fst.allocate(10);
+    test_block(block,10);
+    REQUIRE(fst.owns(block));
+    Mallocator  snd (std::move(fst));
+    test_block(block,10);
+    REQUIRE(snd.owns(block));
+//    snd = std::move(snd);
+//    test_block(block,10);
+//    REQUIRE(snd.owns(block)); TODO:should work?
+    fst = Mallocator();
+    snd.deallocate(block);
+    test_null(block);
+    block = fst.allocate(10);
+    test_block(block,10);
+    snd = std::move(fst);
+    test_block(block,10);
+    REQUIRE(snd.owns(block));
+}
