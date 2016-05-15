d45a2b94348171a15720735dfbfa9751b5ffaa20
kejsty <k.kejstova@gmail.com>
2016-05-15 23:17:08 +0200

    deleted benchmark

--- bench/bench_freelist.cpp
+++ bench/bench_freelist.cpp
@@ -41,29 +41,5 @@ static void bench_freelist_dealloc(benchmark::State& s) {
     }
 }
 
-static void bench_freelist_randomValues(benchmark::State& s) {
-    Freelist<Mallocator, 8, 4 * 1024> alloc;
-    std::mt19937 gen;
-    std::uniform_int_distribution<int> dist(8, s.range_x());
-    std::vector<Block> allocations(1000);
-
-    while (s.KeepRunning()) {
-        for (int i = 0; i < 1000; ++i) {
-            if (allocations[i].ptr == nullptr) {
-                int toAllocate = dist(gen);
-                allocations[i] = alloc.allocate(toAllocate);
-            } else {
-                alloc.deallocate(allocations[i]);
-            }
-        }
-        for (int i = 0; i < 1000; ++i) {
-            if (allocations[i].ptr != nullptr) {
-                alloc.deallocate(allocations[i]);
-            }
-        }
-    }
-}
-
 BENCHMARK(bench_freelist_alloc)->Range(8, 1024 * 1024);
 BENCHMARK(bench_freelist_dealloc)->Range(8, 1024 * 1024);
-BENCHMARK(bench_freelist_randomValues)->Range(8, 1024 * 1024);
