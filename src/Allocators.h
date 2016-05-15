7387c4480a00240f73387c80d9d8c66f4ba29a53
xlauko <xlauko@mail.muni.cz>
2016-05-15 10:48:35 +0200

    Added Allocators header

---
+++ src/Allocators.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Block.h"
+
+#include "AffixAllocator.h"
+//#include "BitmappedBlock.h"
+//#include "Bucketizer.h"
+#include "FallbackAllocator.h"
+#include "Freelist.h"
+#include "Mallocator.h"
+#include "NullAllocator.h"
+#include "Segregator.h"
+#include "StackAllocator.h"
+#include "StatisticCollector.h"
