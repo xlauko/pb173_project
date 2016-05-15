1b7c72f7a33bfcdac6d8f4fbd066ede50b3fca35
Jiří Novotný <novotng@gmail.com>
2016-05-16 01:50:14 +0200

    Memory leak fixed

--- bench/bench_stack.cpp
+++ bench/bench_stack.cpp
@@ -1,39 +1,40 @@
-#include <benchmark/benchmark.h>
-#include "bench_common.h"
 #include "../src/StackAllocator.h"
+#include "bench_common.h"
+#include <benchmark/benchmark.h>
 #include <iostream>
+#include <memory>
 
 using namespace allocators;
 
 static void bench_stack_alloc(benchmark::State& s) {
-    StackAllocator<1024 * 32 * batch_size, 8> alloc;
+    auto alloc = std::make_unique<StackAllocator<1024 * 32 * batch_size, 8>>();
     std::array<Block, batch_size> blocks;
 
     while (s.KeepRunning()) {
         for (auto& blk : blocks) {
-            blk = alloc.allocate(s.range_x());
+            blk = alloc->allocate(s.range_x());
         }
         s.PauseTiming();
-        for (size_t i = batch_size; i-- > 0;) {
-            alloc.deallocate(blocks[i]);
+        for (size_t i = batch_size - 1; i != size_t(-1); --i) {
+            alloc->deallocate(blocks[i]);
         }
         s.ResumeTiming();
     }
 }
 
 static void bench_stack_dealloc(benchmark::State& s) {
-    StackAllocator<1024 * 32 * batch_size, 8> alloc;
+    auto alloc = std::make_unique<StackAllocator<1024 * 32 * batch_size, 8>>();
     std::array<Block, batch_size> blocks;
 
     while (s.KeepRunning()) {
         s.PauseTiming();
         for (auto& blk : blocks) {
-            blk = alloc.allocate(s.range_x());
+            blk = alloc->allocate(s.range_x());
         }
         s.ResumeTiming();
 
-        for (size_t i = batch_size; i-- > 0;) {
-            alloc.deallocate(blocks[i]);
+        for (size_t i = batch_size - 1; i != size_t(-1); --i) {
+            alloc->deallocate(blocks[i]);
         }
     }
 }
