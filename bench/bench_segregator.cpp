3adb794d60a03300a26679082d6078f2edfd5487
xlauko <xlauko@mail.muni.cz>
2016-06-16 22:03:13 +0200

    Bench: Segregator update

--- bench/bench_segregator.cpp
+++ bench/bench_segregator.cpp
@@ -7,39 +7,52 @@
 
 using namespace allocators;
 
-static void bench_segregator_alloc(benchmark::State& s) {
-    Segregator<128, Mallocator, Freelist<Mallocator, 128, 256, 1024>> alloc;
-    std::array<Block, batch_size> blocks;
+template <size_t size> void bench_segregator_alloc(benchmark::State& s) {
+	Segregator<8, Mallocator, Freelist<Mallocator, 128, size, 1024>> alloc;
+	std::array<Block, batch_size> blocks;
 
-    while (s.KeepRunning()) {
-        for (auto& blk : blocks) {
-            blk = alloc.allocate(s.range_x());
-        }
+	while (s.KeepRunning()) {
+		for (auto& blk : blocks) {
+			blk = alloc.allocate(size);
+		}
 
-        s.PauseTiming();
-        for (auto& blk : blocks) {
-            alloc.deallocate(blk);
-        }
-        s.ResumeTiming();
-    }
+		s.PauseTiming();
+		for (auto& blk : blocks) {
+			alloc.deallocate(blk);
+		}
+		s.ResumeTiming();
+	}
 }
 
-static void bench_segregator_dealloc(benchmark::State& s) {
-    Segregator<128, Mallocator, Freelist<Mallocator, 128, 256, 1024>> alloc;
-    std::array<Block, batch_size> blocks;
+template <size_t size> void bench_segregator_dealloc(benchmark::State& s) {
+	Segregator<8, Mallocator, Freelist<Mallocator, 128, size, 1024>> alloc;
+	std::array<Block, batch_size> blocks;
 
-    while (s.KeepRunning()) {
-        s.PauseTiming();
-        for (auto& blk : blocks) {
-            blk = alloc.allocate(s.range_x());
-        }
-        s.ResumeTiming();
+	while (s.KeepRunning()) {
+		s.PauseTiming();
+		for (auto& blk : blocks) {
+			blk = alloc.allocate(size);
+		}
+		s.ResumeTiming();
 
-        for (auto& blk : blocks) {
-            alloc.deallocate(blk);
-        }
-    }
+		for (auto& blk : blocks) {
+			alloc.deallocate(blk);
+		}
+	}
 }
 
-BENCHMARK(bench_segregator_alloc)->Range(8, 1024 * 1024);
-BENCHMARK(bench_segregator_dealloc)->Range(8, 1024 * 1024);
+BENCHMARK_TEMPLATE(bench_segregator_alloc, 8);
+BENCHMARK_TEMPLATE(bench_segregator_alloc, 64);
+BENCHMARK_TEMPLATE(bench_segregator_alloc, 512);
+BENCHMARK_TEMPLATE(bench_segregator_alloc, 4 * 1024);
+BENCHMARK_TEMPLATE(bench_segregator_alloc, 32 * 1024);
+BENCHMARK_TEMPLATE(bench_segregator_alloc, 256 * 1024);
+BENCHMARK_TEMPLATE(bench_segregator_alloc, 1024 * 1024);
+
+BENCHMARK_TEMPLATE(bench_segregator_dealloc, 8);
+BENCHMARK_TEMPLATE(bench_segregator_dealloc, 64);
+BENCHMARK_TEMPLATE(bench_segregator_dealloc, 512);
+BENCHMARK_TEMPLATE(bench_segregator_dealloc, 4 * 1024);
+BENCHMARK_TEMPLATE(bench_segregator_dealloc, 32 * 1024);
+BENCHMARK_TEMPLATE(bench_segregator_dealloc, 256 * 1024);
+BENCHMARK_TEMPLATE(bench_segregator_dealloc, 1024 * 1024);
